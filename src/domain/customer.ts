4a977f9ce5401b09ab9597a36e66425e9ec38551
Okiemute Esiri <okiemuteesiri214@gmail.com>
2026-09-06 12:54:24 +0100

    feat: add customer domain model

---
+++ src/domain/customer.ts
@@ -0,0 +1,19 @@
+export type CustomerId = string;
+
+export interface Customer {
+  id: CustomerId;
+  name: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CreateCustomerInput {
+  name: string;
+  email: string;
+}
+
+export interface UpdateCustomerInput {
+  name?: string;
+  email?: string;
+}
